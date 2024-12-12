7d9f618f19e842a29132ab3c17aa304afa16e30b
naomie1288 <128naomie12@gmail.com>
2024-12-12 18:48:35 +0200

    new

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello")
+def prime_number(num,list):
+    sum=0
+    for y in range(1,10000):
+        if num%y==0:
+            sum+=1
+    if sum==2:
+        list.append(num)
+        return list
+
+def main():
+    list = []
+    for i in range(1, 10000):
+        list = prime_number(i,list)
+    print(list)
+main()
